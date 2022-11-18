1c379af0019ff61798854f4464a90b92376a5dff
21whatsup <97479324+21whatsup@users.noreply.github.com>
2022-11-18 10:40:53 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,112 @@
+answer = input("do you wanna play a game of adventure?  [yes/no]")
+
+
+if answer == "yes":
+  print("that's great lets start.")
+
+
+  print(" you will have five level's let's start with level one threw thesse level's you will collect item's and tresure's to beat the special final boss ok")
+
+  answer = input("you run into a giant owlbear will fight or run? [fight/run]")
+
+  if answer == "run":
+     print("you made it to the next level")
+
+  if answer == "fight":
+    print("you beat the owlbear and get a a battle axe level 1")
+
+  print("level 2")
+
+  answer = input("you come near a group of goblin's what will you do. [fight/battleaxe and eat/run]")
+
+  if answer == "run":
+    print("you get caught and lose 12 hp")
+
+
+  if answer == "fight":
+    print("you get 1 healing potion")
+
+
+  if answer == "battleaxe and eat":
+    print("congragulation's on your meal")
+
+
+  print("level three")
+
+  answer = input("you run into the wizard of the underworld what will you do? [fight/run]")
+
+  if answer == "fight":
+      print("you lose 10 hp")
+
+  if answer == "run":
+      print("Game Over you will restart")
+
+  answer = input("you have time to heal will you? [heal/continue]")
+
+  if answer == "heal":
+    print("you gain all your health")
+
+  if answer == "continue":
+    print("okay then werido")
+
+
+  print("level 4")
+
+  answer = input("you come a cross a alien cenipede what will you do? [fight/run]")
+
+  if answer == "fight":
+    print("you get hurt but kill it you lost 12 hp and get a healing potion")
+
+  if answer == "run":
+    print("you get caught and lose 30 hp")
+
+  answer == input("before you go to the final level will you heal or cointinue or upgrade your wepon?  [heal/upgrade]")
+
+  if answer == "heal":
+    print("your health is at 100 hp")
+
+  if answer == "upgrade":
+    print("your battle axe is level 100")
+
+  print("level 5")
+
+  answer = input("you run into the final boss a world serpant it swallows you hole  left with 1 hp will you cut your way out or give up? [cut out/give up]")
+
+
+  if answer == "cut out":
+    print("you beat the boss and saved all of the realms thank you for playing")
+
+  if answer == "give up":
+    print(" we'll i know who won't be usefull in battles what a quiter you discipointment")
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+  if answer == "no":
+    print("why are you even here")
+
+    
